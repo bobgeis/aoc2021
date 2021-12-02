5b2444ee81be010b80906775a195dad02527cf3a
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-02 11:33:14 -0500

    d03: init

---
+++ day/d03.nim
@@ -0,0 +1,23 @@
+import lib/[imps]
+# https://adventofcode.com/2021/day/3
+
+const
+  day = "03"
+  inPath = inputPath(day)
+  t1path = inputPath(day,"t1")
+
+inpath.part1is 1
+inpath.part2is 2
+t1path.part1is 1
+t1path.part2is 2
+
+proc part0*(path: string): seq[string] =
+  path.getLines
+
+proc part1*(input: seq[string]): int =
+  result = 1
+
+proc part2*(input: seq[string]): int =
+  result = 2
+
+makeRunProc(day)
