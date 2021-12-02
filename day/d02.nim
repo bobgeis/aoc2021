0a8085771e6f4f75dd6f69f4a09e107602ccf3f2
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-02 09:07:36 -0500

    d02: done

--- day/d02.nim
+++ day/d02.nim
@@ -1,22 +1,48 @@
 import lib/[imps]
 
 const
-  day = "00"
+  day = "02"
   inPath = inputPath(day)
-  # t1path = inputPath(day,"t1")
+  t1path = inputPath(day,"t1")
 
-# inpath.part1is 1
-# inpath.part2is 2
-# t1path.part1is 1
-# t1path.part2is 2
+inpath.part1is 2019945
+inpath.part2is 1599311480
+t1path.part1is 150
+t1path.part2is 900
 
-proc part0*(path: string): seq[string] =
-  path.getLines
+type Move = tuple
+  dir: Vec2i
+  amt: int
 
-proc part1*(input: seq[string]): int =
-  result = 0
+proc parseLine(s:string):Vec2i =
+  ## looks like: "up 5"
+  ## directions are: forward, up, down
+  var
+    dir: string
+    amt: int
+  if s.scanf("$w $i", dir, amt):
+    result = case dir:
+      of "up": [0,-1]
+      of "forward": [1,0]
+      of "down": [0,1]
+      else: err &"Unknown direction: {dir}"; [0,0]
+    result *= amt
 
-proc part2*(input: seq[string]): int =
-  result = 0
+proc part0*(path: string): seq[Vec2i] =
+  path.withLines:
+    result.add line.parseLine
+
+proc part1*(input: seq[Vec2i]): int =
+  let dest = input.foldl(a + b)
+  result = dest[0] * dest[1]
+
+proc part2*(input: seq[Vec2i]): int =
+  var
+    dest = [0,0]
+    aim = 0
+  for move in input:
+    aim += move[1]
+    dest += [move[0], aim * move[0]]
+  result = dest[0] * dest[1]
 
 makeRunProc(day)
