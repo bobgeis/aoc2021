8aca31995d70d2e6f6c2879b3f20fbd0ed30f24f
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-02 19:11:23 -0500

    d01 edit comments

--- day/d01.nim
+++ day/d01.nim
@@ -22,7 +22,7 @@ proc part1*(input: seq[int]): int =
 proc part2*(input: seq[int]): int =
   result = 0
   when defined(d01oldmethod):
-    # the old method actually adds them up, run with:
+    # the old method adds them up, run with:
     # nim dr -d:d01oldmethod d01
     var
       curr = input[0] + input[1] + input[2]
@@ -32,10 +32,9 @@ proc part2*(input: seq[int]): int =
       curr = input[i] + input[i-1] + input[i-2]
       if curr > last: inc result
   else:
-    # the old method actually compared the sums
-    # but this is unnecessary
-    # because the sums being compared are adjacent
-    # so they share some numbers which can be subtracted out
+    # summing is actually unnecessary
+    # because the sums being compared
+    # share some numbers which can be subtracted out
     # leaving just the first number of the old sum
     # and the last number of the new sum
     for i in 3..input.high:
