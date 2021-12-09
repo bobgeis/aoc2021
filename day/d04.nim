13c3221b938fd41db156cf612a796127e5cf7288
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-08 20:08:18 -0500

    d04: part 2

--- day/d04.nim
+++ day/d04.nim
@@ -8,15 +8,16 @@ const
   t1path = inputPath(day,"t1")
 
 inpath.part1is 63552
-# inpath.part2is 2
+inpath.part2is 9020
 t1path.part1is 4512
-# t1path.part2is 2
+t1path.part2is 1924
 
 type
   Board = object
     board: Table[int,Vec2i]
     unmarked: HashSet[int]
     xs,ys: Vec5i
+    done:bool
 
 proc initBoard(ints: seq[int]): Board =
   var
@@ -25,10 +26,14 @@ proc initBoard(ints: seq[int]): Board =
   for idx,i in ints:
     unmarked.incl i
     board[i] = [idx mod 5, idx div 5]
-  return Board(board:board, unmarked:unmarked, xs:[0,0,0,0,0], ys:[0,0,0,0,0])
+  return Board(board:board, unmarked:unmarked, xs:[0,0,0,0,0], ys:[0,0,0,0,0],done:false)
 
-proc part0*(path: string): seq[seq[int]] =
-  result = path.readFile.split("\n\n").mapit(it.findAll(re"-?\d+").mapit(it.parseint))
+proc part0*(path: string): (seq[int],seq[Board]) =
+  let ints = path.readFile.split("\n\n").mapit(it.findAll(re"-?\d+").mapit(it.parseint))
+  var boards: seq[Board] = @[]
+  for idx in 1..ints.high:
+    boards.add initBoard(ints[idx])
+  (ints[0],boards)
 
 proc score(board:Board,num:int):int =
   var sum = board.unmarked.toSeq.sum
@@ -36,25 +41,36 @@ proc score(board:Board,num:int):int =
 
 proc update(board:var Board,num:int):int =
   if num notin board.unmarked: return 0
+  if board.done: return 0
   board.unmarked.excl num
   let v = board.board[num]
   inc board.xs[v.x]
   inc board.ys[v.y]
   if board.xs.anyit(it == 5) or board.ys.anyit(it == 5):
+    board.done = true
     return num * board.unmarked.toSeq.sum
   return 0
 
-proc part1*(input: seq[seq[int]]): int =
-  var boards:seq[Board] = @[]
-  for idx in 1..input.high:
-    boards.add initBoard(input[idx])
-  for num in input[0]:
+proc propagate(ints:seq[int],boards: sink seq[Board]): (int,int) =
+  var
+    numleft = boards.len
+    first,last:int
+  for num in ints:
     for board in boards.mitems:
-      let i:int = board.update(num)
-      if i > 0: return i
-  return 0
+      let i = board.update(num)
+      if i > 0:
+        if numleft == boards.len:
+          first = i
+        elif numleft == 1:
+          last = i
+          return (first,last)
+        dec numleft
+  (0,0)
 
-proc part2*(input: seq[seq[int]]): int =
-  result = 2
+proc part1*(input: (seq[int],seq[Board])): int =
+  return propagate(input[0],input[1])[0]
+
+proc part2*(input: (seq[int],seq[Board])): int =
+  return propagate(input[0],input[1])[1]
 
 makeRunProc(day)
