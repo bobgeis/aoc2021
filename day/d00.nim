62bbd2c30eba427baa799ad26e9479262e678c06
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-13 19:26:11 -0500

    d07: p1 and p2

--- day/d00.nim
+++ day/d00.nim
@@ -17,14 +17,11 @@ proc part2*(input: seq[string]): int =
 const
   inPath = inputPath(day)
   t1path = inputPath(day,"t1")
-  o1path = inputPath(day,"o1")
 
 # t1path.part1is 0
 # inpath.part1is 0
-# o1path.part1is 0
 
 # t1path.part2is 0
 # inpath.part2is 0
-# o1path.part2is 0
 
 makeRunProc(day)
--- day/d07.nim
+++ day/d07.nim
@@ -8,23 +8,33 @@ proc part0*(path: string): seq[int] =
   path.readFile.findAll(re"-?\d+").mapit(it.parseint)
 
 proc part1*(input: seq[int]): int =
-  result = 0
-
+  var totals:seq[int] = @[]
+  for i in input.min..input.max:
+    var total = 0
+    for j in input:
+      total += abs(j-i)
+    totals.add total
+  totals.min
 
 proc part2*(input: seq[int]): int =
-  result = 0
+  ## triangular numbers https://en.wikipedia.org/wiki/Triangular_number
+  var totals:seq[int] = @[]
+  for i in input.min..input.max:
+    var total = 0
+    for j in input:
+      let n = abs(j-i)
+      total += (n * (n + 1)) div 2
+    totals.add total
+  totals.min
 
 const
   inPath = inputPath(day)
   t1path = inputPath(day,"t1")
-  o1path = inputPath(day,"o1")
 
 t1path.part1is 37
-# inpath.part1is 0
-# o1path.part1is 0
+inpath.part1is 356992
 
-# t1path.part2is 0
-# inpath.part2is 0
-# o1path.part2is 0
+t1path.part2is 168
+inpath.part2is 101268110
 
 makeRunProc(day)
