cdcf08384d78ab95bb97b4f00449f613f16a0a10
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-13 19:26:07 -0500

    d06: part 1

--- day/d00.nim
+++ day/d00.nim
@@ -12,17 +12,17 @@ proc part0*(path: string): seq[string] =
   path.getLines
 
 proc part1*(input: seq[string]): int =
-  result = 1
+  result = 0
 
-# t1path.part1is 1
-# inpath.part1is 1
-# o1path.part1is 1
+# t1path.part1is 0
+# inpath.part1is 0
+# o1path.part1is 0
 
 proc part2*(input: seq[string]): int =
-  result = 2
+  result = 0
 
-# t1path.part2is 2
-# inpath.part2is 2
-# o1path.part2is 2
+# t1path.part2is 0
+# inpath.part2is 0
+# o1path.part2is 0
 
 makeRunProc(day)
---
+++ day/d06.nim
@@ -0,0 +1,40 @@
+import lib/[imps]
+# https://adventofcode.com/2021/day/6
+
+const
+  day = "06"
+  inPath = inputPath(day)
+  t1path = inputPath(day,"t1")
+  o1path = inputPath(day,"o1")
+
+proc tick(ints: var seq[int]) =
+  var toadd = 0
+  for idx,i in ints:
+    if i == 0:
+      ints[idx] = 6
+      inc toadd
+    else:
+      dec ints[idx]
+  for a in 1..toadd:
+    ints.add 8
+
+proc part0*(path: string): seq[int] =
+  path.readFile.findAll(re"-?\d+").mapit(it.parseint)
+
+proc part1*(input: sink seq[int]): int =
+  for i in 1..80:
+    input.tick
+  result = input.len
+
+t1path.part1is 5934
+inpath.part1is 385391
+# o1path.part1is 1
+
+proc part2*(input: seq[int]): int =
+  result = 0
+
+# t1path.part2is 2
+# inpath.part2is 2
+# o1path.part2is 2
+
+makeRunProc(day)
