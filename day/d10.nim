7850f66f02922ddb6e772b559a546300994ed77e
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-19 19:34:41 -0500

    d10: par 2, correct but ugly

--- day/d10.nim
+++ day/d10.nim
@@ -3,12 +3,20 @@ import lib/[imps]
 
 const day = "10"
 
-const scoretab = {
+const
+  errscore = {
     ')' : 3,
     ']' : 57,
     '}' : 1197,
     '>' : 25137,
   }.toTable()
+  compscore = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+  }.toTable()
+
 
 proc part0*(path: string): seq[string] =
   path.getLines
@@ -24,13 +32,36 @@ proc part1*(input: seq[string]): int =
       elif c == '[': expect.add ']'
       elif c == expect[^1]: discard expect.pop
       else:
-        errs.add (expect[^1],c,idx,scoretab[c],line)
+        errs.add (expect[^1],c,idx,errscore[c],line)
         break
   result = errs.reduce((acc:int,e) => acc + e[3],0)
 
 
+proc score(s:seq[char]):int =
+  var idx = s.len
+  while idx > 0:
+    dec idx
+    result *= 5
+    result += compscore[s[idx]]
+
 proc part2*(input: seq[string]): int =
-  result = 0
+  var completions: seq[seq[char]] = @[]
+  for line in input:
+    var
+      err = false
+      expect: seq[char] = @[]
+    for idx,c in line:
+      if c == '(': expect.add ')'
+      elif c == '<': expect.add '>'
+      elif c == '{': expect.add '}'
+      elif c == '[': expect.add ']'
+      elif c == expect[^1]: discard expect.pop
+      else:
+        err = true
+        break
+    if not err:
+      completions.add expect
+  completions.mapit(it.score).sorted[completions.len div 2]
 
 const
   inPath = inputPath(day)
@@ -40,7 +71,7 @@ t1path.part1is 26397
 inpath.part1is 370407
 
 t1path.part2is 288957
-# inpath.part2is 0
+inpath.part2is 3249889609
 
 makeRunProc(day)
 
