c4b2cfbc92670679d6e3280d472cf37759d2f0fd
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-19 19:34:38 -0500

    d10: init

---
+++ day/d10.nim
@@ -0,0 +1,30 @@
+import lib/[imps]
+# https://adventofcode.com/2021/day/10
+
+const day = "10"
+
+
+proc part0*(path: string): seq[string] =
+  path.getLines
+
+proc part1*(input: seq[string]): int =
+  result = 0
+
+
+proc part2*(input: seq[string]): int =
+  result = 0
+
+const
+  inPath = inputPath(day)
+  t1path = inputPath(day,"t1")
+
+t1path.part1is 26397
+# inpath.part1is 0
+
+# t1path.part2is 0
+# inpath.part2is 0
+
+makeRunProc(day)
+
+when isMainModule:
+  getCliPaths(day).doit(it.run.echoRR)
