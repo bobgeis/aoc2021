dbe5f6f8d07211de74d974c0e2b486f4807a6d6d
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-20 19:27:13 -0500

    d14: part 2, needs cleanup

--- day/d14.nim
+++ day/d14.nim
@@ -32,8 +32,29 @@ proc part1*(input: (string,Table[string,string])): int =
   result = counts[^1] - counts[0]
 
 
+## Brute force does NOT work for part 2! we need to do something smarter...
+## We don't actually care about the order of the char in the string, just how many times each char appears.
+## The last time this happened, a CountTable was the way to go.
+
+proc tick(pairs:CountTable[string],maptab:Table[string,string]):CountTable[string] =
+  for k,v in pairs:
+    result.inc k[0] & maptab[k], v
+    result.inc maptab[k] & k[1], v
+
 proc part2*(input: (string,Table[string,string])): int =
-  result = 0
+  let
+    maptab = input[1]
+    templ = input[0]
+  var ctab = initCountTable[string]()
+  for idx in 0..templ.high-1:
+    ctab.inc templ[idx..idx+1]
+  for i in 1..40:
+    ctab = ctab.tick(maptab)
+  var score = initCountTable[char]()
+  score.inc templ[^1] # count that last char of the template string
+  for k,v in ctab:
+    score.inc k[0], v
+  return score.largest.val - score.smallest.val
 
 const
   inPath = inputPath(day)
@@ -42,8 +63,8 @@ const
 t1path.part1is 1588
 inpath.part1is 2170
 
-# t1path.part2is 2188189693529
-# inpath.part2is 0
+t1path.part2is 2188189693529
+inpath.part2is 2422444761283
 
 makeRunProc(day)
 
