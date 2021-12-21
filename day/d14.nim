63591bea387f362c087f3278635a5abd301acb7e
Bob Geis <bobgeis@users.noreply.github.com>
2021-12-20 19:27:11 -0500

    d14: part 1

--- day/d14.nim
+++ day/d14.nim
@@ -4,14 +4,35 @@ import lib/[imps]
 const day = "14"
 
 
-proc part0*(path: string): seq[string] =
-  path.getLines
+proc part0*(path: string): (string,Table[string,string]) =
+  let
+    parts = path.readfile.split("\n\n")
+    templ = parts[0]
+  var pairs = inittable[string,string]()
+  for line in parts[1].splitlines:
+    let (_, base, insert) = line.scantuple("$w -> $w")
+    pairs[base] = insert
+  (templ,pairs)
 
-proc part1*(input: seq[string]): int =
-  result = 0
+proc part1*(input: (string,Table[string,string])): int =
+  let pairs = input[1]
+  var
+    prev = input[0]
+    next = ""
+  for i in 1..10:
+    next = ""
+    for idx in 0..prev.high-1:
+      next.add prev[idx]
+      next.add pairs[prev[idx..idx+1]]
+    next.add prev[^1].toString
+    prev = next
+  var ctab = initCountTable[char]()
+  for c in next: ctab.inc(c)
+  let counts = ctab.values.toseq.sorted
+  result = counts[^1] - counts[0]
 
 
-proc part2*(input: seq[string]): int =
+proc part2*(input: (string,Table[string,string])): int =
   result = 0
 
 const
@@ -19,9 +40,9 @@ const
   t1path = inputPath(day,"t1")
 
 t1path.part1is 1588
-# inpath.part1is 0
+inpath.part1is 2170
 
-# t1path.part2is 0
+# t1path.part2is 2188189693529
 # inpath.part2is 0
 
 makeRunProc(day)
